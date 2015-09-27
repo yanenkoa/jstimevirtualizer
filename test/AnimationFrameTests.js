aa693dfa308c1578fa4332bf3402843b9b2289fe
Alexandr Yanenko <yanenkoalexandr@gmail.com>
2015-09-27 17:32:57 +0300

    Add test for 'cancelAnimationFrame'

--- test/AnimationFrameTests.js
+++ test/AnimationFrameTests.js
@@ -3,7 +3,7 @@ describe("AnimationFrameRequest", function() {
         timeVirtualizer.virtualize();
     });
 
-    it("works", function() {
+    it("triggers required number of times when FPS is set", function() {
         var afCallback = jasmine.createSpy("AF callback");
         requestAnimationFrame(afCallback);
         timeVirtualizer.setFPS(60);
@@ -12,7 +12,22 @@ describe("AnimationFrameRequest", function() {
         expect(afCallback.calls.count()).not.toBeLessThan(60);
     });
 
+    it("disappears after calling cancelAnimationFrame", function() {
+        var afCallback = jasmine.createSpy("AF callback");
+        var frameID = requestAnimationFrame(afCallback);
+        timeVirtualizer.setFPS(60);
+
+        timeVirtualizer._advanceTimeMSInSafeContext(1000);
+        expect(afCallback.calls.count()).not.toBeLessThan(60);
+        var prevCallCount = afCallback.calls.count();
+
+        cancelAnimationFrame(frameID);
+        timeVirtualizer._advanceTimeMSInSafeContext(1000);
+        expect(afCallback.calls.count()).toBe(prevCallCount);
+    });
+
     afterAll(function() {
         timeVirtualizer.setFPS(0);
+        timeVirtualizer.unVirtualize();
     });
 });
