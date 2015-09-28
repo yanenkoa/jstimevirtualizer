d96e556910f20573975898b511cb7fcfdf28fb19
Alexandr Yanenko <yanenkoalexandr@gmail.com>
2015-09-29 02:21:56 +0300

    Make AnimationFrame tests more strict

--- test/AnimationFrameTests.js
+++ test/AnimationFrameTests.js
@@ -5,11 +5,19 @@ describe("AnimationFrameRequest", function() {
 
     it("triggers required number of times when FPS is set", function() {
         var afCallback = jasmine.createSpy("AF callback");
-        requestAnimationFrame(afCallback);
+        var frameID = requestAnimationFrame(afCallback);
+
+        var timeoutCallback = jasmine.createSpy("timeout callback");
+        var intervalID = setInterval(timeoutCallback, 10);
+
         timeVirtualizer.setFPS(60);
 
         timeVirtualizer._advanceTimeMSInSafeContext(1000);
-        expect(afCallback.calls.count()).not.toBeLessThan(60);
+        expect(afCallback.calls.count()).
+            toBe(Math.floor(1000 / Math.floor(1000 / 60)));
+
+        clearInterval(intervalID);
+        cancelAnimationFrame(frameID);
     });
 
     it("triggers once when FPS is not set", function() {
@@ -24,15 +32,22 @@ describe("AnimationFrameRequest", function() {
     it("disappears after calling cancelAnimationFrame", function() {
         var afCallback = jasmine.createSpy("AF callback");
         var frameID = requestAnimationFrame(afCallback);
+
+        var timeoutCallback = jasmine.createSpy("timeout callback");
+        var intervalID = setInterval(timeoutCallback, 10);
+
         timeVirtualizer.setFPS(60);
 
         timeVirtualizer._advanceTimeMSInSafeContext(1000);
-        expect(afCallback.calls.count()).not.toBeLessThan(60);
+        expect(afCallback.calls.count()).
+            toBe(Math.floor(1000 / Math.floor(1000 / 60)));
         var prevCallCount = afCallback.calls.count();
 
         cancelAnimationFrame(frameID);
         timeVirtualizer._advanceTimeMSInSafeContext(1000);
         expect(afCallback.calls.count()).toBe(prevCallCount);
+
+        clearInterval(intervalID);
     });
 
     afterAll(function() {
