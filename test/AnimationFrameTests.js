43c6906584e4b9e05d59a17109c680cf6679138c
Alexandr Yanenko <yanenkoalexandr@gmail.com>
2015-09-29 01:29:49 +0300

    Add test for behaviour when FPS is not set

--- test/AnimationFrameTests.js
+++ test/AnimationFrameTests.js
@@ -12,6 +12,15 @@ describe("AnimationFrameRequest", function() {
         expect(afCallback.calls.count()).not.toBeLessThan(60);
     });
 
+    it("triggers once when FPS is not set", function() {
+        timeVirtualizer.setFPS(0);
+        var afCallback = jasmine.createSpy("AF callback");
+        requestAnimationFrame(afCallback);
+
+        timeVirtualizer._advanceTimeMSInSafeContext(1000);
+        expect(afCallback.calls.count()).toBe(1);
+    });
+
     it("disappears after calling cancelAnimationFrame", function() {
         var afCallback = jasmine.createSpy("AF callback");
         var frameID = requestAnimationFrame(afCallback);
